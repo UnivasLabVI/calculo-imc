34a5ccba12351874968d97caaaa6efc9cfca43ae
Alfredo Eduardo <alfredudu_007@hotmail.com>
2015-09-09 00:05:59 +0000

    Upload

--- services/bmi.action.php
+++ services/bmi.action.php
@@ -1,12 +1,15 @@
-<? php
+<?php
+require 'BMIService.class.php';
 
-$weight = $_POST ['weight'];
-$height = $_POST ['height'];$_POST
+$weight = $_GET['weight'];
+$height = $_GET['height'];
 $bmiIndex = 0.0;
 
-if (is_numeric($weight) && is_numeric($height)){
+if(is_numeric($weight) && is_numeric($height)) {
     $bmiIndex = BMIService::getIndex($weight, $height);
 }
+
+sleep (1);
 
 echo $bmiIndex;
 ?>
